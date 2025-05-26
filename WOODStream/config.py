74a41a1499143fa2e54a29fc57ccff32ee41f0e8
xroidmanga <theseltian@gmail.com>
2025-05-26 09:23:53 +0530

    Update config.py

--- WOODStream/config.py
+++ WOODStream/config.py
@@ -16,8 +16,8 @@ class Telegram:
     FORCE_SUB = env.get('FORCE_UPDATES_CHANNEL', False)
     FORCE_SUB = True if str(FORCE_SUB).lower() == "true" else False
     SLEEP_THRESHOLD = int(env.get("SLEEP_THRESHOLD", "60"))
-    FILE_PIC = env.get('FILE_PIC', "https://graph.org/file/fff4ea35f47dce9626c69.jpg")
-    START_PIC = env.get('START_PIC', "https://graph.org/file/fff4ea35f47dce9626c69.jpg")
+    FILE_PIC = env.get('FILE_PIC', "https://i.ibb.co/NnKTp1kd/IMG-20250526-092053-167.jpg")
+    START_PIC = env.get('START_PIC', "https://i.ibb.co/NnKTp1kd/IMG-20250526-092053-167.jpg")
     VERIFY_PIC = env.get('VERIFY_PIC', "https://graph.org/file/736e21cc0efa4d8c2a0e4.jpg")
     MULTI_CLIENT = False
     FLOG_CHANNEL = int(env.get("FLOG_CHANNEL", None))   # Logs channel for file logs
