28481b40908270478dbfa041b9669e9718526395
xroidmanga <theseltian@gmail.com>
2025-05-26 13:30:40 +0530

    Update config.py

--- WOODStream/config.py
+++ WOODStream/config.py
@@ -13,7 +13,7 @@ class Telegram:
     UPDATES_CHANNEL = str(env.get('UPDATES_CHANNEL', "usefull_botz"))
     SESSION_NAME = str(env.get('SESSION_NAME', 'WOODStream'))
     FORCE_SUB_ID = env.get('FORCE_SUB_ID', None)
-    FORCE_SUB = env.get('FORCE_UPDATES_CHANNEL', True)
+    FORCE_SUB = env.get('FORCE_UPDATES_CHANNEL', False)
     FORCE_SUB = True if str(FORCE_SUB).lower() == "true" else False
     SLEEP_THRESHOLD = int(env.get("SLEEP_THRESHOLD", "60"))
     FILE_PIC = env.get('FILE_PIC', "https://i.ibb.co/NnKTp1kd/IMG-20250526-092053-167.jpg")
